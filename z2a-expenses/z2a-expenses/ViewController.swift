443e61a698f9ce3510e0158b3114f08b863e8611
Mike McDonald <mpmcdonald@google.com>
2019-04-25 14:25:23 -0700

    Initial commit of iOS code

---
+++ z2a-expenses/z2a-expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  z2a-expenses
+//
+//  Created by Mike McDonald on 4/23/19.
+//  Copyright © 2019 Firebase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

f16bc72f926810767a61f1e965a1a0a7b0ded614
Frank van Puffelen <puf@google.com>
2019-04-25 13:49:46 -0700

    Fixed layout, added auth button, simplified some code

--- android/app/src/main/java/com/firebase/expensetracker/MainActivity.kt
+++ android/app/src/main/java/com/firebase/expensetracker/MainActivity.kt
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.*;
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -38,51 +38,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        //FirebaseAuth.getInstance().signOut()
+        FirebaseAuth.getInstance().addAuthStateListener { _ ->
+            invalidateOptionsMenu()
+        }
 
         fab.setOnClickListener { _ ->
-            if (FirebaseAuth.getInstance().currentUser == null) {
-                val providers = arrayListOf(
-                        AuthUI.IdpConfig.EmailBuilder().build(),
-                        AuthUI.IdpConfig.GoogleBuilder().build())
+            //val intent = Intent(Intent.ACTION_PICK)//, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
 
-                startActivityForResult(
-                        AuthUI.getInstance()
-                                .createSignInIntentBuilder()
-                                .setAvailableProviders(providers)
-                                .build(),
-                        RC_SIGN_IN)
-            }
-            else {
-                //val intent = Intent(Intent.ACTION_PICK)//, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //val intent = Intent(Intent.ACTION_GET_CONTENT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //val intent = Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA)
+            // 1. Show file picker
+            val getImageIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getImageIntent.type = "image/jpeg"
+            startActivityForResult(getImageIntent, RC_PHOTO_PICKER)
 
-                //val intent = Intent(Intent.ACTION_GET_CONTENT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                //val intent = Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA)
-                // 1. Show file picker
-                val getImageIntent = Intent(Intent.ACTION_GET_CONTENT)
-                getImageIntent.type = "image/jpeg"
-                startActivityForResult(getImageIntent, RC_PHOTO_PICKER)
+            // 2. Show camera
+            //val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            //startActivityForResult(cameraIntent, RC_PHOTO_PICKER)
+            // TODO: the result code for this doesn't work yet
 
-                // 2. Show camera
-                //val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-                //startActivityForResult(cameraIntent, RC_PHOTO_PICKER)
-                // TODO: the result code for this doesn't work yet
-
-                // 3. Show chooser
-                //val chooser = Intent.createChooser(getImageIntent, "Select Picture")
-                //chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(cameraIntent))
-                //startActivityForResult(chooser, RC_PHOTO_PICKER)
-
-            }
+            // 3. Show chooser
+            //val chooser = Intent.createChooser(getImageIntent, "Select Picture")
+            //chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(cameraIntent))
+            //startActivityForResult(chooser, RC_PHOTO_PICKER)
         }
 
         val userDocRef = FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().currentUser?.uid ?: "yes")
         userDocRef.addSnapshotListener { documentSnapshot, _ ->
             if (documentSnapshot != null && documentSnapshot.exists()) {
-                val userAmountView = this.findViewById<TextView>(R.id.user_amount)
-                val teamAmountView = this.findViewById<TextView>(R.id.team_amount)
-                userAmountView.text = roundAmount(documentSnapshot.get("user_cost")!!).toString()
-                teamAmountView.text = roundAmount(documentSnapshot.get("team_cost")!!).toString()
+                var text = formatAmount(documentSnapshot.get("user_cost"))
+                if (!user_amount.text.equals(text)) {
+                    showMessage("My cost was updated to "+text)
+                    user_amount.text = text
+                }
+
+                text = formatAmount(documentSnapshot.get("team_cost"))
+                if (!team_amount.text.equals(text)) {
+                    showMessage("Team cost was updated to "+text)
+                    team_amount.text = text
+                }
             }
         }
 
@@ -134,24 +128,59 @@ class MainActivity : AppCompatActivity() {
     }
     private fun showMessage(msg: Any?) {
         if (msg != null) {
-            Snackbar.make(fab, msg.toString() ?: "", Snackbar.LENGTH_LONG)
+            Snackbar.make(fab, msg.toString(), Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
 
     private fun showExpense(data: Map<String, Any>) {
         val amountView = this.findViewById<TextView>(R.id.amount)
-        val currencyView = this.findViewById<TextView>(R.id.currency)
-        val receiptView = this.findViewById<ImageView>(R.id.receipt)
+        //val currencyView = this.findViewById<TextView>(R.id.currency)
+        //val receiptView = this.findViewById<ImageView>(R.id.receipt)
         showMessage("Found amount ${data["item_cost"]}")
-        amountView.text = roundAmount(data["item_cost"]!!).toString()
+        amountView.text = formatAmount(data["item_cost"])
         //if (data.containsKey("currency")) currencyView.text = data["currency"].toString();
-        if (data.contains("receipt")) Glide.with(this).load(data["receipt"]).into(receiptView);
+        //if (data.contains("receipt")) Glide.with(this).load(data["receipt"]).into(receiptView);
     }
-    private fun roundAmount(amount: Any): Double {
-        return roundAmount(amount as Double)
+
+    private fun roundAmount(amount: Any): Double { return roundAmount(amount as Double) }
+    private fun roundAmount(amount: Double): Double { return Math.round(amount * 100.0) / 100.0 }
+    private fun formatAmount(amount: Any?): String {
+        return String.format("%10.2f", amount as Double)
     }
-    private fun roundAmount(amount: Double): Double{
-        return Math.round(amount * 100.0) / 100.0
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val title = if (FirebaseAuth.getInstance().currentUser == null) "Sign in" else "Sign out"
+        //var icon =  if (FirebaseAuth.getInstance().currentUser == null) R.drawable.ic_person_outline_white_24dp else R.drawable.ic_person_outline_white_24dp
+        menu!!.findItem(R.id.menu_auth).title = title
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_auth -> {
+                if (FirebaseAuth.getInstance().currentUser == null) {
+                    val providers = arrayListOf(
+                            AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.GoogleBuilder().build())
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            RC_SIGN_IN)
+                }
+                else {
+                    AuthUI.getInstance().signOut(this)
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

a714144bd5a3ff087d20291054bea1060cd3a43b
Kat Fang <katfang@katfang-macbookpro.roam.corp.google.com>
2019-04-26 16:58:55 -0700

    using capital letter

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const path = require('path');
 const admin = require('firebase-admin');
-const vision = require('@google-cloud/vision');
+const Vision = require('@google-cloud/vision');
 
 const visionClient = Vision({
   projectId: 'z2a-expenses'
